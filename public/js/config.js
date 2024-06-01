d07fc0b9b4f9bce334ba807406a0d9c42bea7f52
Railer Chalbaud <railer.chalbaud@gmail.com>
2024-06-01 12:55:52 -0400

    UPDATE: v1.0.4 se enlazaron los proyectos al front y se actualizo el bucle que carga los demos en la ventana modal de proyectos a partir del config.json

--- public/js/config.js
+++ public/js/config.js
@@ -1,27 +1,22 @@
 const config = {
     test: "Config Obj loaded.",
-    secciones: [
+    proyectos: [
         {
-            nombre: "Sobre mi",
-            texto: "Técnico profesional, metódico, responsable, autodidacta y proactivo con más de tres (3) años de experiencia laboral en diseño, maquetación y desarrollo web con conocimientos avanzados de tecnologías como Javascript, PHP, SQL, Node, React, HTML, CSS, jQuery, Bootstrap. Ya sea una API, un sitio web, una PWA, un sistema de gestión empresarial, sistemas de encuesta, etc. puedo ayudar a tu equipo a cumplir sus objetivos ofreciendo soluciones efectivas y escalables desarrollando proyectos que permitan un manejo eficiente de la información y cumplan con los objetivos solicitados, todo esto bajo el marco del compromiso, el respeto y la comunicación. Como desarrollador full stack he desarrollado proyectos desde cero partiendo del levantamiento del requerimiento seguido de la etapa diseño, desarrollo, testeo, puesta en producción, monitoreo y actualización de aplicaciones, he trabajado bajo metodología SCRUM, he liderado proyectos y equipos de desarrollo apostando a la buena comunicación y el respeto para lograr la eficiencia y eficacia deseadas.Mi filosofía de trabajo se enfoca en la proactividad y reactividad, eficiencia y compromiso con cada proyecto, generar un código limpio, ordenado, escalable y funcional aunado a un buen diseño de UI sin dejar de lado la UX es parte importante en cada desarrollo para cumplir con lo solicitado.",
-            img: []
+            nombre: "PWA",
+            url: "projects/01_demoPWA_(Paises)/index.html",
+            img: "projects/01_demoPWA_(Paises)/thumbnail.jpg"
         },
         {
-            nombre: "Front End",
-            img: []
+            nombre: "Drag & Drop",
+            url: "projects/02_Drag&Drop_(KanbanTest)/index.html",
+            img: "projects/02_Drag&Drop_(KanbanTest)/thumbnail.jpg"
         },
         {
-            nombre: "Back End",
-            img: []
+            nombre: "IndexedDB",
+            url: "projects/03_browserCRUD_(indexedDB)/index.html",
+            img: "projects/03_browserCRUD_(indexedDB)/thumbnail.jpg"
         },
-        {
-            nombre: "Portafolio",
-            img: []
-        },
-        {
-            nombre: "contacto",
-            img: []
-        }
+
     ]
 }
 
